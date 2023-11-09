35e72bd9284e59d81b26d925eb9f19404280722a
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-11-09 17:17:06 +0500

    added redux , made some monipulation

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import Header from '../Header/Header'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from '../../store/counter/counterSlice'
+
+
 export default function About() {
+
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+
   return (
     <>
       <main className='about'  >
         <Header />
         <section style={{ width: "100%", height: '100%', display: "flex", justifyContent: "center", alignItems: "center" }}>
-
-          <button>
+          <button onClick={() => dispatch(increment())}>
             +
           </button>
           <h1>
-            0
+            {count}
           </h1>
-          <button>
+          <button onClick={() => dispatch(decrement())}>
             -
           </button>
         </section>
